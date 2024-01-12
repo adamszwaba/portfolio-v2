7f432ff901c3eb9fd32d0141e36713b0f2c1929f
Adam Szwaba <szwaba.adam@gmail.com>
2024-01-12 20:25:49 +0100

    swap logo

--- src/features/page-contents/app-bar.tsx
+++ src/features/page-contents/app-bar.tsx
@@ -7,17 +7,10 @@ const AppBar: React.FC = () => {
     <nav className="not-prose w-full p-2 shadow-md dark:bg-slate-900 bg-slate-100">
       <div className="container flex items-center justify-between m-auto">
         <NextLink href="/" className="flex items-center">
-          <Logo width={24} height={24} />
-          <span className="border-l-stone-600 border-l ml-2 h-6 pl-2 flex-grow leading-6 hover:underline">
+          <Logo height={64} width={64} />
+          <span className=" ml-2 h-16 pl-2 flex-grow leading-[4rem] hover:underline font-bold text-2xl">
             @ Adam Szwaba
           </span>
-        </NextLink>
-        <NextLink
-          href="https://github.com/adamszwaba/portfolio"
-          className="bg-slate-200 px-3 py-1 dark:bg-slate-700 dark:hover:bg-slate-600 hover:bg-slate-300 transition-colors duration-200 rounded-md leading-6 flex items-center gap-1"
-        >
-          <FaGithub width={24} height={24} />
-          Source
         </NextLink>
       </div>
     </nav>
--- src/features/page-contents/logo.tsx
+++ src/features/page-contents/logo.tsx
@@ -1,51 +1,34 @@
 const Logo = ({ ...props }: React.SVGProps<SVGSVGElement>) => {
   return (
-    <svg viewBox="0 0 85 92" {...props}>
-      <mask
-        id="mask0"
-        mask-type="alpha"
-        maskUnits="userSpaceOnUse"
-        x="0"
-        y="43"
-        width="43"
-        height="49"
-      >
-        <rect
-          y="43.7483"
-          width="42.5"
-          height="48.2517"
-          className="dark:fill-slate-100 fill-slate-400"
-        />
-      </mask>
-      <g mask="url(#mask0)">
-        <path
-          d="M42.5 46.6542L85 68.8454L42.5 91.0365L0 68.8454L42.5 46.6542Z"
-          className="dark:fill-slate-50 fill-slate-900"
-        />
-      </g>
-      <mask
-        id="mask1"
-        mask-type="alpha"
-        maskUnits="userSpaceOnUse"
-        x="0"
-        y="0"
-        width="85"
-        height="92"
-      >
-        <rect width="85" height="92" className="dark:fill-slate-100 fill-slate-400" />
-      </mask>
-      <g mask="url(#mask1)">
-        <path
-          d="M42.5 0.686859L85 22.3496L42.5 44.0124L0 22.3496L42.5 0.686859Z"
-          className="dark:fill-slate-50 fill-slate-900"
-        />
-        <path
-          d="M0 21.8213L42.5 44.0124L0 66.2036L-42.5 44.0124L0 21.8213Z"
-          className="fill-slate-600 dark:fill-slate-50"
-        />
-      </g>
-    </svg>
-  );
-};
+    <svg xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="0 0 60 90" {...props}>
+      <circle cx="0" cy="24.97" r="2.22" fill="#ffcc00" />
+      <circle cx="17.33" cy="16.19" r="1.04" fill="#ffcc00" />
+      <circle cx="26.04" cy="28.66" r="1.75" fill="#ffcc00" />
+      <circle cx="20.12" cy="49.80" r="1.5" fill="#ffcc00" />
+      <circle cx="35.23" cy="79.41" r="2.33" fill="#ffcc00" />
+      <circle cx="6.43" cy="84.52" r="1.57" fill="#ffcc00" />
+      <circle cx="12.48" cy="57.63" r="1.71" fill="#ffcc00" />
+      <circle cx="51.90" cy="15.10" r="0.54" fill="#ffcc00" />
+      <circle cx="55.69" cy="19.42" r="0.66" fill="#ffcc00" />
+      <circle cx="55.45" cy="31.00" r="0.93" fill="#ffcc00" />
+      <circle cx="52.96" cy="42.14" r="0.92" fill="#ffcc00" />
+      <circle cx="49.25" cy="44.73" r="0.61" fill="#ffcc00" />
 
-export default Logo;
+      <line x1="0" y1="24.97" x2="17.33" y2="16.19" stroke="#e5e7eb99" strokeWidth="1" />
+      <line x1="17.33" y1="16.19" x2="26.04" y2="28.66" stroke="#e5e7eb99" strokeWidth="1" />
+      <line x1="26.04" y1="28.66" x2="20.12" y2="49.80" stroke="#e5e7eb99" strokeWidth="1" />
+      <line x1="20.12" y1="49.80" x2="35.23" y2="79.41" stroke="#e5e7eb99" strokeWidth="1" />
+      <line x1="35.23" y1="79.41" x2="6.43" y2="84.52" stroke="#e5e7eb99" strokeWidth="1" />
+      <line x1="6.43" y1="84.52" x2="12.48" y2="57.63" stroke="#e5e7eb99" strokeWidth="1" />
+      <line x1="12.48" y1="57.63" x2="0" y2="24.97" stroke="#e5e7eb99" strokeWidth="1" />
+      <line x1="26.04" y1="28.66" x2="55.53" y2="26.32" stroke="#e5e7eb99" strokeWidth="1" />
+      <line x1="55.53" y1="26.32" x2="51.90" y2="15.10" stroke="#e5e7eb99" strokeWidth="1" />
+      <line x1="51.90" y1="15.10" x2="55.69" y2="19.42" stroke="#e5e7eb99" strokeWidth="1" />
+      <line x1="55.69" y1="19.42" x2="55.45" y2="31.00" stroke="#e5e7eb99" strokeWidth="1" />
+      <line x1="55.45" y1="31.00" x2="52.96" y2="42.14" stroke="#e5e7eb99" strokeWidth="1" />
+      <line x1="52.96" y1="42.14" x2="49.25" y2="44.73" stroke="#e5e7eb99" strokeWidth="1" />
+    </svg>
+  )
+}
+
+export default Logo
