adf2e2fc2ebb1b1db9c7e2af500afcd0101e934b
Adam Szwaba <szwaba.adam@gmail.com>
2024-01-21 00:11:14 +0100

    Add SpeedInsights component to RootLayout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,11 +16,11 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <SpeedInsights />
       <body className={inter.className}>
         <AppBar />
         <main className="container mx-auto py-4">{children}</main>
         <Footer />
+        <SpeedInsights />
       </body>
     </html>
   )
