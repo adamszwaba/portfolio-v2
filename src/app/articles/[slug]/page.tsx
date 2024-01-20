dfacceb262534fab3a91bbc0acce35b88e9abb5d
Adam Szwaba <szwaba.adam@gmail.com>
2024-01-20 19:20:10 +0100

    Add metadata

--- src/app/articles/[slug]/page.tsx
+++ src/app/articles/[slug]/page.tsx
@@ -1,5 +1,5 @@
 import { allArticles } from 'contentlayer/generated'
-import { NextPage } from 'next'
+import { Metadata, NextPage } from 'next'
 import { useMDXComponent } from 'next-contentlayer/hooks'
 import { notFound } from 'next/navigation'
 import MDXProviderComponents from '@/features/page-contents/mdx-components'
@@ -7,6 +7,25 @@ import MDXProviderComponents from '@/features/page-contents/mdx-components'
 export async function generateStaticParams() {
   const slugs = allArticles.map((article) => article.slug)
   return slugs.map((slug) => ({ slug }))
+}
+
+export async function generateMetadata({
+  params: { slug },
+}: {
+  params: { slug: string }
+}): Promise<Metadata> {
+  const article = allArticles.find((article) => article.slug === `/articles/${slug}`)
+  if (!article) notFound()
+  return {
+    title: article.title,
+    description: article.excerpt,
+    openGraph: {
+      type: 'article',
+      authors: ['Adam Szwaba'],
+      publishedTime: article.publishedAt,
+      description: article.excerpt,
+    },
+  }
 }
 
 const ArticlePage: NextPage<{ params: { slug: string } }> = async ({ params }) => {
