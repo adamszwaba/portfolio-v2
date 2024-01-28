1465787b4e1c2a3e28720f07b6ea48ac6c100b34
Adam Szwaba <szwaba.adam@gmail.com>
2024-01-28 17:45:47 +0100

    Fix error with skill display

--- src/features/about-me/stack/skill-display.tsx
+++ src/features/about-me/stack/skill-display.tsx
@@ -1,21 +1,23 @@
-import { StackItem } from "./stack-item";
+import { StackItem } from './stack-item'
+import { clsx } from 'clsx'
 
 const SkillDisplay = (stackItem: StackItem) => {
-  const width = {
-    beginner: "1/4",
-    intermediate: "1/2",
-    advanced: "3/4",
-    expert: "full",
-  }[stackItem.level];
   return (
     <div className="flex flex-col">
-      <h4 className="font-bold">{stackItem.name.toUpperCase()}</h4>
-      <div className="mt-auto h-2 overflow-hidden rounded-full bg-gray-700">
-        <div className={`h-full bg-green-500 w-${width}`} />
+      <h4 className="font-bold truncate text-sm mb-1">{stackItem.name.toUpperCase()}</h4>
+      <div className="h-2 overflow-hidden rounded-full dark:bg-gray-600 bg-gray-300">
+        <div
+          className={clsx('h-full bg-green-500 rounded', {
+            'w-full': stackItem.level === 'expert',
+            'w-3/4': stackItem.level === 'advanced',
+            'w-1/2': stackItem.level === 'intermediate',
+            'w-1/4': stackItem.level === 'beginner',
+          })}
+        />
       </div>
-      <p className="text-xs">{stackItem.level}</p>
+      <p className="mt-1 text-xs uppercase text-white">{stackItem.level}</p>
     </div>
-  );
-};
+  )
+}
 
-export default SkillDisplay;
+export default SkillDisplay
--- src/features/about-me/stack/stack.ts
+++ src/features/about-me/stack/stack.ts
@@ -1,115 +1,119 @@
-import { StackItem } from "./stack-item";
+import { StackItem } from './stack-item'
 
-export type Stack = Record<string, StackItem[]>;
+export type Stack = Record<string, StackItem[]>
 
 export const stack: Stack = {
   languages: [
     {
-      name: "Typescript",
-      level: "expert",
+      name: 'Typescript',
+      level: 'expert',
     },
     {
-      name: "Javascript",
-      level: "expert",
+      name: 'Javascript',
+      level: 'expert',
     },
     {
-      name: "golang",
-      level: "intermediate",
+      name: 'golang',
+      level: 'intermediate',
     },
   ],
-  "Web frameworks": [
+  'Web frameworks': [
     {
-      name: "React",
-      level: "expert",
+      name: 'React',
+      level: 'expert',
     },
     {
-      name: "Next.js",
-      level: "expert",
+      name: 'Next.js',
+      level: 'expert',
     },
-    { name: "Remix", level: "advanced" },
+    { name: 'Remix', level: 'advanced' },
     {
-      name: "Vite",
-      level: "intermediate",
+      name: 'Vite',
+      level: 'intermediate',
     },
     {
-      name: "Angular",
-      level: "beginner",
+      name: 'Angular',
+      level: 'beginner',
     },
   ],
   testing: [
     {
-      name: "Jest",
-      level: "expert",
+      name: 'Jest',
+      level: 'expert',
     },
     {
-      name: "React Testing Library",
-      level: "expert",
+      name: 'React Testing Library',
+      level: 'expert',
     },
     {
-      name: "Cypress",
-      level: "intermediate",
+      name: 'Cypress',
+      level: 'intermediate',
     },
     {
-      name: "vitest",
-      level: "advanced",
-    },
-  ],
-  "State management": [
-    {
-      name: "Redux/Redux Toolkit",
-      level: "expert",
-    },
-    {
-      name: "React Query",
-      level: "expert",
-    },
-    {
-      name: "Zustand",
-      level: "intermediate",
+      name: 'vitest',
+      level: 'advanced',
     },
   ],
-  "CSS frameworks": [
+  'State management': [
     {
-      name: "Material UI",
-      level: "expert",
+      name: 'Redux/Redux Toolkit',
+      level: 'expert',
     },
     {
-      name: "Tailwind CSS",
-      level: "expert",
+      name: 'React Query',
+      level: 'expert',
     },
     {
-      name: "Chakra UI",
-      level: "intermediate",
+      name: 'Zustand',
+      level: 'intermediate',
+    },
+  ],
+  'CSS frameworks': [
+    {
+      name: 'Material UI',
+      level: 'expert',
     },
     {
-      name: "Styled Components",
-      level: "intermediate",
+      name: 'Tailwind CSS',
+      level: 'expert',
+    },
+    {
+      name: 'Radix UI',
+      level: 'expert',
+    },
+    {
+      name: 'Chakra UI',
+      level: 'intermediate',
+    },
+    {
+      name: 'Styled Components',
+      level: 'intermediate',
     },
   ],
   Other: [
     {
-      name: "GraphQL",
-      level: "intermediate",
+      name: 'GraphQL',
+      level: 'intermediate',
     },
     {
-      name: "Docker",
-      level: "intermediate",
+      name: 'Docker',
+      level: 'intermediate',
     },
     {
-      name: "Git",
-      level: "advanced",
+      name: 'Git',
+      level: 'advanced',
     },
     {
-      name: "prisma",
-      level: "intermediate",
+      name: 'prisma',
+      level: 'intermediate',
     },
     {
-      name: "drizzleORM",
-      level: "intermediate",
+      name: 'drizzleORM',
+      level: 'intermediate',
     },
     {
-      name: "d3.js",
-      level: "intermediate",
+      name: 'd3.js',
+      level: 'intermediate',
     },
   ],
-};
+}
