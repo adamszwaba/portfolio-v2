afc854cf7a1033754a08c58e24aa5ca86d08534c
Adam Szwaba <adam.szwaba@prometheanworld.com>
2024-07-09 17:17:59 +0200

    Add inner child to list

--- src/app/comics/page.tsx
+++ src/app/comics/page.tsx
@@ -6,6 +6,7 @@ const ComicsPage = () => {
       <Link href="/comics/1">1: Law of conservation</Link>
       <Link href="/comics/2">2: CSS hacks</Link>
       <Link href="/comics/3">3: Hospitality</Link>
+      <Link href="/comics/4">4: Inner Child</Link>
     </ul>
   );
 };
